34106d0847d65d5f9bd00be063e8f40714c34eb3
dxwwan <kkw1700@gmail.com>
2024-04-08 16:49:53 +0900

    feat: 시작 버튼 추가

---
+++ src/components/home/ProcessButton.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const ProcessButton = () => {
+  return (
+    <ProcessButtonContainer>
+      <PlayImg src="../../../src/assets/startImg.png" alt="play" />
+      <ProcessButtonContent>Get Started</ProcessButtonContent>
+    </ProcessButtonContainer>
+  );
+};
+
+const ProcessButtonContainer = styled.button`
+  display: flex;
+  justify-content: center;
+  width: 20rem;
+  height: 4rem;
+  margin-top: 2.5rem;
+  border: none;
+  align-items: center;
+  border-radius: 1.25rem;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+`;
+
+const ProcessButtonContent = styled.p`
+  font-size: 2rem;
+  color: #5d5d5d;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const PlayImg = styled.img`
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1rem;
+`;
