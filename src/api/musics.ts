b55d0fa178f4e6f4920f2d6cbabaf01d73a6e130
dxwwan <kkw1700@gmail.com>
2024-04-16 10:52:15 +0900

    feat: 음악 상수 추가

---
+++ src/api/musics.ts
@@ -0,0 +1,81 @@
+export interface PlayListDetailProps {
+  id: number;
+  title: string;
+  img: string;
+  src: string;
+}
+
+export const MUSICS: PlayListDetailProps[] = [
+  {
+    id: 1,
+    title: 'Seasons',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 2,
+    title: 'Summer',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 3,
+    title: 'Spring',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 4,
+    title: 'Fall',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 5,
+    title: 'Winter',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 4,
+    title: 'WonderWall',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 5,
+    title: "Don't Look Back In Anger",
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 6,
+    title: 'Champagne Supernova',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 7,
+    title: 'Live Forever',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 8,
+    title: 'Hello',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 9,
+    title: 'Rolling In The Deep',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+  {
+    id: 10,
+    title: 'Someone Like You',
+    img: '../../../src/assets/musicImg.svg',
+    src: '../../../src/assets/audio/sumu - apart [NCS Release].mp3',
+  },
+];
