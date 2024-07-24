3e3d41f4dc74a5fc8342caa7bc85725f93d8d1f8
dxwwan <silicagel6624@gmail.com>
2024-07-24 12:47:23 +0900

    feat: 로딩 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet } from 'react-router-dom';
 import theme from './style/theme';
+import { Suspense } from 'react';
+import { Loading } from './components/Loading';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Suspense fallback={Loading} />
       <QueryClientProvider client={queryClient}>
         <Outlet />
         <ReactQueryDevtools initialIsOpen={false} />
