e4224506cc1ecfc00e92339974ee5a81b38803c4
dxwwan <silicagel6624@gmail.com>
2024-07-24 16:08:53 +0900

    feat: 이미지 생성 디자인 수정

--- src/components/create/CreateImage.tsx
+++ src/components/create/CreateImage.tsx
@@ -1,6 +1,7 @@
 import { useGenerateQuery } from '../../hooks/Query/useGenerateQuery';
 import styled from 'styled-components';
 import React, { useState } from 'react';
+import imageUpload from '../../assets/imageUpload.svg';
 
 export const CreateImage = () => {
   const [formData, setFormData] = useState<FormData>(new FormData());
@@ -8,8 +9,19 @@ export const CreateImage = () => {
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const image = e.target.files![0];
     const formData = new FormData();
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        alert('사진이 성공적으로 업로드되었습니다!');
+      };
+      reader.readAsDataURL(image);
+    }
     formData.append('image', image);
     setFormData(formData);
+  };
+
+  const addImage = () => {
+    document.querySelector('input')?.click();
   };
 
   const submitImage = () => {
@@ -23,7 +35,14 @@ export const CreateImage = () => {
         accept="image/*"
         onChange={handleChangeInput}
       />
-      <CreateImageButton onClick={submitImage}>이미지 업로드</CreateImageButton>
+      <CreateInputImage
+        src={imageUpload}
+        alt="imageUpload"
+        onClick={addImage}
+      />
+      <CreateImageButton onClick={submitImage}>
+        사진으로 음악 생성하기
+      </CreateImageButton>
     </CreateImageContainer>
   );
 };
@@ -31,10 +50,11 @@ export const CreateImage = () => {
 const CreateImageContainer = styled.div`
   display: flex;
   width: 40rem;
-  height: 30rem;
+  height: 20rem;
   margin: 2rem 0 2rem 2rem;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   border-radius: 0.875rem;
   border: 1px solid #e2d2fe;
   background: #fff;
@@ -42,14 +62,25 @@ const CreateImageContainer = styled.div`
 `;
 
 const CreateImageInput = styled.input`
-  display: block;
+  display: none;s
+`;
+
+const CreateInputImage = styled.img`
+  width: 5rem;
+  height: 5rem;
+  src: url(${imageUpload});
+  cursor: pointer;
 `;
 
 const CreateImageButton = styled.button`
-  width: 10rem;
-  height: 3rem;
-  margin-top: 1rem;
   border: none;
-  border-radius: 1.25rem;
+  background: #fff;
   cursor: pointer;
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin-top: 2rem;
+  color: #000;
+  letter-spacing: 0.125rem;
+  text-transform: capitalize;
+  border-radius: 0.875rem;
 `;
