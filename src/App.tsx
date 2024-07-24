40402c8eb287c0e0c98abd7db0d54eb712519618
dxwwan <silicagel6624@gmail.com>
2024-07-24 12:58:23 +0900

    feat: 빌드 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet } from 'react-router-dom';
 import theme from './style/theme';
 import { Suspense } from 'react';
-import { Loading } from './components/Loading';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Suspense fallback={Loading} />
+      <Suspense fallback={<div>Loading...</div>} />
       <QueryClientProvider client={queryClient}>
         <Outlet />
         <ReactQueryDevtools initialIsOpen={false} />
